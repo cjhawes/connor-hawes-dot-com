d9f979651905682fa61da94e13b84c64e13bb22d
Connor <cjhawes.cs@gmail.com>
2022-02-09 11:08:50 +0000

    Made email work

--- web/src/app/layouts/modals/email-modal.component.ts
+++ web/src/app/layouts/modals/email-modal.component.ts
@@ -1,6 +1,4 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
-import { EmailService } from 'src/app/services/contact/email.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'email-modal',
@@ -9,24 +7,9 @@ import { EmailService } from 'src/app/services/contact/email.service';
 })
 export class EmailModalComponent implements OnInit {
 
-  public formData!: FormGroup;
-
-  @ViewChild('closebutton') closebutton: any;
-
-  constructor(private builder: FormBuilder, private emailService: EmailService) { }
+  constructor() { }
 
   ngOnInit(): void {
-    this.formData = this.builder.group({
-      Fullname: new FormControl('', [Validators.required]),
-      Email: new FormControl('', [Validators.required, Validators.email]),
-      Comment: new FormControl('', [Validators.required])
-    });
-  }
-
-  onSubmit(formData: FormGroup) {
-    console.log(formData)
-    this.emailService.sendMail(formData);
-    this.closebutton.nativeElement.click();
   }
 
 }
